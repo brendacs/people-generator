cece8d6dfd68c5afe3f93ec673f40e1c477971cb
Brenda Zhang <brenda.zhang2@gmail.com>
2017-02-27 10:55:09 -0800

    Add and style images of people

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 function getData() {
 	$.ajax({
-		url: 'https://randomuser.me/api/?results=10',
+		url: 'https://randomuser.me/api/?results=6',
 		dataType: 'json',
 		success: function(data) {
 			let results = data.results;
-			var ul = document.getElementById('people-list');
 			var i = 0;
 			var people = [];
 				
 			$('div#people-div').empty(); // empty and then append to replace
+			
 			$.each(results, function(data) {
+				people.push('<div>');
+				people.push('<img src="' + results[i].picture.large + '"/>');
 				people.push('<li class="people">' + results[i].name.first + ' ' + results[i].name.last + '</li>');
+				people.push('</div>');
 				i++;
 			});
 
@@ -18,6 +21,9 @@ function getData() {
 				'id': 'people-list',
 				html: people.join('')
 			}).appendTo('div#people-div');
+
+			// for testing
+			// console.log(data);
 	  	}
 	});
 }
